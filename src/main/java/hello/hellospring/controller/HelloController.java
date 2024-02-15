9f28153be89a11a14a3a8876794057f831d20ee4
seoyeon1123 <woghks072@gmail.com>
2024-02-16 08:13:34 +0900

    feat: service-test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!!!");
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
         return "hello";
-    };
+    }
+
+    ;
+
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
     }
@@ -27,44 +30,22 @@ public class HelloController {
 
     @GetMapping("hello-api")
     @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name){
+    public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello();
         hello.setName(name);
         return hello;
     }
-    static class Hello{
-        private  String name;
-        public String getName(){
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
             return name;
         }
-        public void setName(String name){
+
+        public void setName(String name) {
             this.name = name;
         }
 
     }
-    @GetMapping("diarys")
-    @ResponseBody
-    public Diary getDiary(@RequestParam("email") String email){
-        Diary diary = new Diary();
-        diary.setEmail(email);
-        return diary;
-    }
-    static class Diary{
-        private String email;
-        private String tittle;
-
-        public String getEmail(){
-            return email;
-        }
-        public void setEmail(String email){
-            this.email = email;
-        }
-        public String getTittle(){
-            return tittle;
-        }
-        public void setTittle(String tittle){
-            this.tittle = tittle;
-        }
-    }
-
 }
