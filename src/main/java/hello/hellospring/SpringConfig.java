eea0c8527cf3082d37bbeca7e789117864b01650
seoyeon1123 <woghks072@gmail.com>
2024-02-18 16:45:51 +0900

    feat: JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,9 +1,8 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,19 +11,23 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    private final MemberRepository memberRepository;
+
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository(){
-        return new JdbcMemberRepository(dataSource);
-        //return  new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        //return new JdbcMemberRepository(dataSource);
+//        //return  new MemoryMemberRepository();
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//    }
 
 }
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,10 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 
